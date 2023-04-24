1c794f4c9d7fee54f4e54207fb2468f76d177046
Mariusz.lemanski <mariusz.lemanski@daw-systems.pl>
2023-04-24 15:16:30 +0200

    test

---
+++ src/app/pages/new-password/new-password.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewPasswordComponent } from './new-password.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewPasswordComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewPasswordModule { }
