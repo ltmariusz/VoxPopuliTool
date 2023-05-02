1923e681612e56cfdaec490f935f29bba9f90d0b
Mariusz.lemanski <mariusz.lemanski@daw-systems.pl>
2023-05-02 15:47:35 +0200

    ad

---
+++ src/app/pages/admin-add-user/admin-add-user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminAddUserComponent } from './admin-add-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminAddUserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminAddUserModule { }
---
+++ src/app/pages/admin-user-list/admin-user-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminUserListComponent } from './admin-user-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminUserListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminUserListModule { }
