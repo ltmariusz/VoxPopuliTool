e75f379554958a7a560f01c5c421b02a28695c3c
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-05-08 15:59:21 +0200

    change

--- src/app/pages/admin-panel-page/admin-panel-page.module.ts
+++ src/app/pages/admin-panel-page/admin-panel-page.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { AdminPanelPageComponent } from './admin-panel-page.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersListModule } from 'src/app/components/users-list/users-list.module';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 
 const routes: Routes = [
   { path: '', component: AdminPanelPageComponent }
@@ -15,7 +18,9 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    UsersListModule
+    UsersListModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
     AdminPanelPageComponent
