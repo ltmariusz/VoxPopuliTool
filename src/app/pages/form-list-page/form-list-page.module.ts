1309a591140869a017a4eabb47e3be10942b7102
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-05-15 10:38:10 +0200

    add questionnaires list

--- src/app/pages/form-list-page/form-list-page.module.ts
+++ src/app/pages/form-list-page/form-list-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListPageComponent } from './form-list-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionnairesListModule } from 'src/app/components/questionnaires-list/questionnaires-list.module';
 
 const routes: Routes = [
   { path: '', component: FormListPageComponent }
@@ -14,7 +15,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-
+    QuestionnairesListModule
   ],
   exports:[
     FormListPageComponent
--- src/app/services/global-services/user-data.service.ts
+++ src/app/services/global-services/user-data.service.ts
@@ -9,4 +9,17 @@ export class UserDataService {
   public user?: User
 
   constructor() { }
+
+  getUser(){
+    return this.user
+  }
+  // getName():string|undefined{
+  //   return this.user?.name;
+  // }
+  // getId(): number{
+  //   return this.user!.id
+  // }
+  // isAdmin():boolean|undefined{
+  //   return this.user?.type == 'Admin';
+  // }
 }
