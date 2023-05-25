79d8a4ee8f2c3fe24d615a08299223cd31723a70
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-05-25 13:41:05 +0200

    change ./

--- src/app/components/done-answers-preview-personal-questionnaire/done-answers-preview-personal-questionnaire.component.ts
+++ src/app/components/done-answers-preview-personal-questionnaire/done-answers-preview-personal-questionnaire.component.ts
@@ -24,7 +24,20 @@ export class DoneAnswersPreviewPersonalQuestionnaireComponent implements OnInit{
 
   getDoneAnswers(){
     this.answers = this.allFormsManagementService.exampleDoneAnswerForm
-    console.log(this.answers)
+    // console.log(this.answers)
+  }
+
+  checkValueAnswer(answer: any, indexAnswer: number): any{
+    // console.log(answer.allAnswers)
+    console.log(indexAnswer)
+    console.log(answer.correct)
+    let result: boolean
+    for (let index = 0; index < answer.correct.length; index++) {
+      if (indexAnswer == answer.correct[index]) {
+        return true
+      }
+    }
+
   }
 
 }
