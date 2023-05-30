5cbfb32ef426efd9e32fb622993ab1b909888a3e
Mariusz.lemanski <mariusz.lemanski@daw-systems.pl>
2023-05-30 12:40:10 +0200

    dodano folder z tesowymi formularzami

---
+++ src/app/pages/test-pages/example-rate-visit/example-rate-visit.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AllFormsManagementService } from 'src/app/services/management/all-forms-management.service';
+import { OneQuestion } from 'src/app/services/management/create-forms-management.service';
+
+@Component({
+  selector: 'app-example-rate-visit',
+  templateUrl: './example-rate-visit.component.html',
+  styleUrls: ['./example-rate-visit.component.scss']
+})
+export class ExampleRateVisitComponent implements OnInit {
+  listOfQuestionToShow!: Array<OneQuestion>
+  titleForm!: string
+  descriptionForm!: string
+
+  constructor(private allFormsManagementService: AllFormsManagementService,
+    private route: ActivatedRoute) { }
+  ngOnInit() {
+    this.titleForm = this.allFormsManagementService.exampleRateVisit.title
+    this.descriptionForm = this.allFormsManagementService.exampleRateVisit.description
+    this.listOfQuestionToShow = this.allFormsManagementService.exampleRateVisit.questions
+  }
+}
