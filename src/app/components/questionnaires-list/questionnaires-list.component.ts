09019f2b74a81246436d87d1c1a93ee3d8385712
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-06-16 15:57:53 +0200

    add deactivate on list (to edit: reload list with filters)

--- src/app/components/questionnaires-list/questionnaires-list.component.ts
+++ src/app/components/questionnaires-list/questionnaires-list.component.ts
@@ -29,6 +29,10 @@ export class QuestionnairesListComponent implements OnInit{
   usersAnkietaPersonalList?: Array<QuestionnaireList>
   customErrorAnkietaList?: string
 
+  subAnkietaDeactivate?: Subscription
+  loadingAnkietaDeactivate = false
+  customErrorAnkietaDeactivate?: string
+
   constructor(
     private router: Router,
     public dialog: MatDialog,
@@ -95,58 +99,37 @@ export class QuestionnairesListComponent implements OnInit{
       enterAnimationDuration,
       exitAnimationDuration,
     });
-    // dialogRef.afterClosed().subscribe(result =>{
-    //   if (result) {
-    //     //console.log(result)
-    //     if (this.addRedirectionForm.valid) {
-    //       this.loading = true;
-          
-    //       let fromEmployeeId = this.addRedirectionForm.get('selected')!.value;
-    //       let toEmployeeId = this.addRedirectionForm.get('selectedSecond')!.value;
-    //       let fromDate = this.addRedirectionForm.get('fromDate')!.value;
-    //       let toDate = this.addRedirectionForm.get('toDate')!.value;
-
-    //         this.subRedirectionCreate = this.redirectionRest.postRedirectionCreate(fromEmployeeId!.id, toEmployeeId!.id, fromDate!, toDate!).subscribe({
-    //           next: (response) => {
-    //             if(response.body){
-    //               this.hideCard.emit();
-                  
-
-    //             }
-    //             else{
-    //               this.customError = 'Brak obiektu odpowiedzi';
-    //               this.loading = false;
-    //             }
-    //           },
-    //           error: (errorResponse) => {
-    //             switch (errorResponse.status) {
-    //               case 400:
-    //               case 401:
-    //               case 403:
-    //               //case 404:
-    //                 this.customError = errorResponse.error.message;
-    //                 this.loading = false;
-    //                 break;
-                
-    //               default:
-    //                 this.customError = 'Błąd serwera'
-    //                 this.loading = false;
-    //                 break;
-    //             }
-    //             // console.log(this.customError);
-    //           },
-    //           complete: () => {
-    //             this.loading = false;
-    //             this.userListManager.listRefresh.next(true);
-    //           }
-    //         }
-    //       )
-    //       }
-    //   }
-    //   else{
-    //     // console.log('Anuluj');
-    //   }
-    // })
+    dialogRef.afterClosed().subscribe(result =>{
+      if (result) {
+        this.loadingAnkietaDeactivate = true
+      this.subAnkietaDeactivate = this.ankietaRest.putAnkietaIdDeactivate(id).subscribe({
+        next: (response) => {
+          if(response.body){
+            this.getAnkietaList()//TO LOAD OLD FILTER
+            this.loadingAnkietaDeactivate = false
+          }
+          else{
+            this.customErrorAnkietaDeactivate = 'Brak obiektu odpowiedzi';
+            this.popupService.errorEmit(this.customErrorAnkietaDeactivate)
+            this.loadingAnkietaDeactivate = false
+          }
+        },
+        error: (errorResponse) => {
+          this.loadingAnkietaDeactivate = false
+          this.customErrorAnkietaDeactivate = errorResponse.error.message
+          this.popupService.errorEmit(this.customErrorAnkietaDeactivate!)
+        },
+        complete: () => {
+          this.loadingAnkietaDeactivate = false;
+        }
+      })
+        
+    
+      }
+      else{
+        // console.log('Anuluj');
+      }
+    })
   }
 
   openDialogCloseQuestionnairePersonal(enterAnimationDuration: string, exitAnimationDuration: string, id: number): void {
