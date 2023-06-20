e6ed69ec9183cd0b9ad65bba6e969d9e2560232b
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2023-06-20 08:36:30 +0200

    fix new password /adminPanel

--- src/app/components/dialogs/new-password-dialog/new-password-dialog.component.ts
+++ src/app/components/dialogs/new-password-dialog/new-password-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { PopupManagementService } from 'src/app/services/management/popup-management.service';
 
 @Component({
   selector: 'app-new-password-dialog',
@@ -11,17 +12,27 @@ export class NewPasswordDialogComponent {
 
   formNewPassword = new FormGroup({
     password: new FormControl<string>('', Validators.required),
+    password2: new FormControl<string>('', Validators.required)
   });
 
-  constructor(public dialogRef: MatDialogRef<NewPasswordDialogComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<NewPasswordDialogComponent>,
+    private popupService: PopupManagementService,
+    ) { }
 
 
   Add(){
-    if (this.formNewPassword.valid) {
+    let password = this.formNewPassword.get('password')!.value
+    let password2 = this.formNewPassword.get('password2')!.value
+
+    if (this.formNewPassword.valid && password == password2) {
       this.dialogRef.close({
         result: true,
         password: this.formNewPassword.get('password')!.value
       }); 
+    }
+    else{
+      this.popupService.errorEmit("Hasła muszą być identyczne")
     }
   }
 
